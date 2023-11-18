fb70dee25a03c825c6c6cd854d2fb3fc224be00d
parul1702 <138119398+parul1702@users.noreply.github.com>
2023-11-18 21:19:51 +0530

    Add files via upload

---
+++ parul.java
@@ -0,0 +1,10 @@
+package file;
+import java.applet.*;
+import java.awt.*;
+public class  parul extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawString("parul yadav ninaniya",100,100);
+}
+}
